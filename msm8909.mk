9977d2616a24fb8198c7dada01f00e36b19d1c67
c_rrajiv <rrajiv@codeaurora.org>
2017-05-31 12:02:02 +0530

    msm8909: Include wificond and wifilogd in the build package
    WiFi needs wificond and wifilogd service to be part of the build
    package.
    
    CRs-Fixed: 2051165
    Change-Id: Ica8b592caa561270f9feb909be0654a32c44148a

--- msm8909.mk
+++ msm8909.mk
@@ -143,6 +143,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     wpa_supplicant_overlay.conf \
     p2p_supplicant_overlay.conf
+#for wlan
+   PRODUCT_PACKAGES += \
+       wificond \
+       wifilogd
 
 # MIDI feature
 PRODUCT_COPY_FILES += \
