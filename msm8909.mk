f1d331054156f9247f386957379f6c6ceab2cdaf
Rajshekar Eashwarappa <reashw@codeaurora.org>
2017-06-08 17:50:34 +0530

    msm8909: Disabling verity and verified boot.
    Change-Id: I6c728015e1309b994c6099c841f7eb049f518708

--- msm8909.mk
+++ msm8909.mk
@@ -203,5 +203,4 @@ PRODUCT_PACKAGES += android.hardware.health@1.0-impl \
                     android.hardware.health@1.0-service \
                     libhealthd.msm
 #Supports verity
-PRODUCT_SUPPORTS_VERITY := true
-PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/bootdevice/by-name/system
+PRODUCT_SUPPORTS_VERITY := false
